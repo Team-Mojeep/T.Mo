dace4acbb816e7f9a28c769cfff6c9da73d4699d
안윤지 <dksdbswl10430@naver.com>
2022-11-16 11:31:47 +0900

    📝 :: 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
+import NotFoundPage from './pages/NotFoundPage';
 import OpenPage from './pages/OpenPage';
+import Header from './components/Header';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<OpenPage/>}/>
+        <Route path='/' element={<OpenPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+
+        <Route element={<Header />}>
+          {/* 헤더가 들어가야 하는 페이지들은 여기에 넣어주세요 :) */}
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
