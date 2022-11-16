59a32eb46990b3cf2749ba4189abe89f5d6bf9b1
안윤지 <dksdbswl10430@naver.com>
2022-11-16 11:32:14 +0900

    📝 :: 헤더  Outlet 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,29 @@
 import * as S from "./styles";
 import { Logo } from "../../assets";
 import { getAccessToken } from "../../utils/Token";
+import { Outlet } from "react-router-dom";
 
 function Header(): JSX.Element {
   const accessToken = getAccessToken();
 
   return (
-    <S.Wrapper>
-      <img src={Logo} />
-      {accessToken ?
-        <S.Btns>
-          <S.Btn>Mypage</S.Btn>
-          <S.Btn>Logout</S.Btn>
-        </S.Btns>
-        :
-        <S.Btns>
-          <S.Btn>Login</S.Btn>
-          <S.Btn>Signup</S.Btn>
-        </S.Btns>
-      }
-    </S.Wrapper>
+    <>
+      <S.Wrapper>
+        <img src={Logo} />
+        {accessToken ?
+          <S.Btns>
+            <S.Btn>Mypage</S.Btn>
+            <S.Btn>Logout</S.Btn>
+          </S.Btns>
+          :
+          <S.Btns>
+            <S.Btn>Login</S.Btn>
+            <S.Btn>Signup</S.Btn>
+          </S.Btns>
+        }
+      </S.Wrapper>
+      <Outlet />
+    </>
   );
 };
 
