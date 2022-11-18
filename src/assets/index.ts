5f6d070a6c1f1e71f21d541fd5f148a49ded8e23
강진현 <kangjin627@gmail.com>
2022-11-18 20:40:40 +0900

    📝:: 로그인 회원가입 눈 이미지 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
 export { default as OpenLogo } from "./imgs/logo.png";
 export { default as RightArrow } from "./imgs/arrow.svg";
 export { default as Logo } from "./imgs/LOGO.svg";
+export {default as Eye} from "./imgs/eye.svg";
+export {default as Eyeoff} from "./imgs/eye-off.svg";
