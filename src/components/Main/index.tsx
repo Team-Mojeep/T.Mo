1080d08db0273f04cf741da0f3a1d8c8f67cef7b
kimgyuha <kimgyuha25@gmail.com>
2022-11-18 22:55:50 +0900

    💄 ::  Main UI 추가

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,90 @@
+import { Search } from "../../assets";
+import * as S from "./styles";
+function Main(): JSX.Element {
+  const data = [
+    {
+      title:
+        "가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하",
+      content: "~~한 프로젝트를 진행합니다.",
+      day: "2022.11.13",
+      people: 6,
+      tag: "웹",
+    },
+    {
+      title: "프로젝트 같이 하실 백엔들 분들 구합니다.",
+      content: "저희는 ~한 프로젝트를 진행하려고 합니다. ~~",
+      day: "2022.11.13",
+      people: 6,
+      tag: "웹",
+    },
+    {
+      title: "프론트 3명 구합니다.",
+      content:
+        "~~한 프로젝트를 진행하려고 합니다. 유능한 프론트 3명 모집합니다.",
+      day: "2022.11.13",
+      people: 6,
+      tag: "웹",
+    },
+    {
+      title: "프로젝트 같이 프론트한국어어쩌구어쩌프론 아아트한국구",
+      content:
+        "~~한 프로젝트를 진행하려고 합니다. 유능젝트를 진행하려고 합니다. 유능젝트를 진행하려고 합니다. 유능젝트를 진행하려고 합니다. 유능한 론트 3명 모집합니합니다. 유능한 론ew트 3명 모집합니합니다. 유능한 론트 3명 모집합니합니다. 유능한 론트 3명 모집합니다.",
+      day: "2022.11.13",
+      people: 6,
+      tag: "웹",
+    },
+    {
+      title: "프로젝트 같이 프론트한국어어쩌구어쩌프론 아아트한국구",
+      content:
+        "~~한 프로젝트를 진행하려고 합니다. 유능젝트를 진행하려고 합니다. 유능젝트를 진행하려고 합니다. 유능젝트를 진행하려고 합니다. 유능한 론트 3명 모집합니합니다. 유능한 론ew트 3명 모집합니합니다. 유능한 론트 3명 모집합니합니다. 유능한 론트 3명 모집합니다.",
+      day: "2022.11.13",
+      people: 6,
+      tag: "웹",
+    },
+  ];
+
+  return (
+    <S.Wrapper>
+      <S.MainTitle>프로젝트</S.MainTitle>
+      <S.Nav>
+        <S.NavRightWrapper>
+          <S.NavTitle>ALL</S.NavTitle>
+          <S.NavTitle>Web</S.NavTitle>
+          <S.NavTitle>App</S.NavTitle>
+          <S.NavTitle>Ect</S.NavTitle>
+        </S.NavRightWrapper>
+
+        <S.NavleftWrapper>
+          <S.NavSearchWrapper>
+            <S.NavSearchIcon src={Search} />
+            <S.NaveSearchInput placeholder="검색" />
+          </S.NavSearchWrapper>
+          <S.NavAddButton>+</S.NavAddButton>
+        </S.NavleftWrapper>
+      </S.Nav>
+
+      <S.Body>
+        {data.map((project) => {
+          return (
+            <S.PWrapper key={project.title}>
+              <S.PTitle>{project.title}</S.PTitle>
+              <S.PHr />
+              <S.Content>
+                <p>{project.content}</p>
+              </S.Content>
+              <S.PDetail>
+                <div>
+                  <S.PDContent>기간: ~{project.day}</S.PDContent>
+                  <S.PDContent>인원: {project.people}명 모집</S.PDContent>
+                </div>
+                <S.PDContent>#{project.tag}</S.PDContent>
+              </S.PDetail>
+            </S.PWrapper>
+          );
+        })}
+      </S.Body>
+    </S.Wrapper>
+  );
+}
+
+export default Main;
---
+++ src/pages/MainPage/index.tsx
@@ -0,0 +1,9 @@
+import Main from "../../components/Main";
+
+function MainPage(): JSX.Element {
+  return (
+      <Main />
+  );
+}
+
+export default MainPage;
