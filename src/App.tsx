32839e56061850515cf8115e2c4a87d2d96c15c4
kimgyuha <kimgyuha25@gmail.com>
2022-12-04 23:02:04 +0900

    📝 :: 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import LoginPage from "./pages/LoginPage";
 import Header from "./components/Header";
 import MainPage from "./pages/MainPage";
 import ApplicationListPage from "./pages/ApplicationListPage";
+import WritePage from "./pages/WritePage";
 
 function App(): JSX.Element {
   return (
@@ -20,6 +21,7 @@ function App(): JSX.Element {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/main" element={<MainPage />} />
+          <Route path="/write" element={<WritePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
