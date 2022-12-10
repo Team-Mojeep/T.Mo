4b4de082f14488b2ed5525263c2730439d38e10c
kimgyuha <kimgyuha25@gmail.com>
2022-12-11 00:56:54 +0900

    📝:: 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import LoginPage from "./pages/LoginPage";
 import Header from "./components/Header";
 import MainPage from "./pages/MainPage";
 import ApplicationListPage from "./pages/ApplicationListPage";
+import ProjectPage from "./pages/ProjectPage";
 
 function App(): JSX.Element {
   return (
@@ -20,6 +21,7 @@ function App(): JSX.Element {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/main" element={<MainPage />} />
+          <Route path="/project" element={<ProjectPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
