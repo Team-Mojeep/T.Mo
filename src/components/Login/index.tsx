52303cea9be5d5cb561a2d40e2c537a441698c1a
안윤지 <dksdbswl10430@naver.com>
2022-12-14 19:55:27 +0900

    ✨ :: 로그인 기능 추가

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,43 +1,78 @@
 import * as S from "./styles";
-import {useState} from "react";
+import { useState } from "react";
 import { Eye } from "../../assets";
 import { Eyeoff } from "../../assets";
-import {AuthType} from "../../interface";
+import { AuthType } from "../../interface";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-function Login(): JSX.Element{
-    const [isSecret, setIsSecret] = useState<boolean>(false);
-    const passwordType = isSecret ? "text" : "password";
+const BASE_URL = "https://192.168.167.105";
 
-    const Text : AuthType[] = [
-        {title : "ID", type:"text"},
-        {title : "Password", type:passwordType}
-    ];
-    return(
-        <S.FlexWrapper>
-            <S.Wrapper>
-                <S.Signup>로그인</S.Signup>
-                {
-                    Text.map((list)=>(
-                        <S.LoginInput 
-                        placeholder={list.title} 
-                        type={list.type}/>
-                    ))}
-                <S.GoBtn>Go</S.GoBtn>
-                <S.Eyes>
-                    {isSecret ? 
-                    <img
-                    onClick={()=> setIsSecret(!isSecret)}
-                    src={Eye}
-                    />
-                    :
-                    <img
-                    onClick={()=> setIsSecret(!isSecret)}
-                    src={Eyeoff}
-                    />}
-                </S.Eyes>
-            </S.Wrapper>
-        </S.FlexWrapper>
-    );
-};
+function Login(): JSX.Element {
+  const [isSecret, setIsSecret] = useState<boolean>(false);
+  const passwordType = isSecret ? "text" : "password";
+  const naigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = inputs;
+
+  const Text: AuthType[] = [
+    { title: "Email", type: "text", name: "email" },
+    { title: "Password", type: passwordType, name: "password" },
+  ];
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const login = async () => {
+    await axios
+      .post(`${BASE_URL}/user/login`, {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        localStorage.setItem("t_mo_access_token", res.data.accessToken);
+      });
+  };
+
+  const onLogin = () => {
+    try {
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <S.FlexWrapper>
+      <S.Wrapper>
+        <S.Signup>로그인</S.Signup>
+        {Text.map((list) => (
+          <S.LoginInput
+            placeholder={list.title}
+            type={list.type}
+            name={list.name}
+            onChange={onChange}
+          />
+        ))}
+        <S.GoBtn onClick={onLogin}>Go</S.GoBtn>
+        <S.Eyes>
+          {isSecret ? (
+            <img onClick={() => setIsSecret(!isSecret)} src={Eye} />
+          ) : (
+            <img onClick={() => setIsSecret(!isSecret)} src={Eyeoff} />
+          )}
+        </S.Eyes>
+      </S.Wrapper>
+    </S.FlexWrapper>
+  );
+}
 
 export default Login;
