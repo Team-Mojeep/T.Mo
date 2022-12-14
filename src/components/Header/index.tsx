7bb6609bb365e6594f43f0d95289c80bebef5e59
안윤지 <dksdbswl10430@naver.com>
2022-12-14 19:54:49 +0900

    📝 :: 프리티어 적용

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import * as S from "./styles";
 import { Logo } from "../../assets";
 import { getAccessToken } from "../../utils/Token";
 import { Link } from "react-router-dom";
-import { Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 function Header(): JSX.Element {
   const accessToken = getAccessToken();
@@ -11,29 +11,29 @@ function Header(): JSX.Element {
     <>
       <S.Wrapper>
         <img src={Logo} />
-        {accessToken ?
+        {accessToken ? (
           <S.Btns>
-            <Link to="/Login" style={{textDecoration: "none"}}>
+            <Link to="/Login" style={{ textDecoration: "none" }}>
               <S.Btn>Mypage</S.Btn>
             </Link>
-            <Link to="/Signup" style={{textDecoration: "none"}}>
+            <Link to="/Signup" style={{ textDecoration: "none" }}>
               <S.Btn>Logout</S.Btn>
             </Link>
           </S.Btns>
-          :
+        ) : (
           <S.Btns>
-            <Link to="/Login" style={{textDecoration: "none"}}>
+            <Link to="/Login" style={{ textDecoration: "none" }}>
               <S.Btn>Login</S.Btn>
             </Link>
-            <Link to="/Signup" style={{textDecoration: "none"}}>
+            <Link to="/Signup" style={{ textDecoration: "none" }}>
               <S.Btn>Signup</S.Btn>
             </Link>
           </S.Btns>
-        }
+        )}
       </S.Wrapper>
       <Outlet />
     </>
   );
-};
+}
 
 export default Header;
